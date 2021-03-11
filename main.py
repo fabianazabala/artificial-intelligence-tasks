a7d8b5d8c1b902bbfa3b5b4ecef23746a065d82c
fabiana.zabala <fabianazabala@hotmail.es>
2021-03-11 20:10:05 +0100

    Array creation.

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+def listTesting():
+    list1 = [1, 2, 3, 4, 5]
+    list2 = [4, 5, 3, 7, 6]
+    list3 = list1 + list2
+    print(list3)
+
+
+def numpyTesting():
+    vec1 = np.array([1, 2, 3, 4, 5])
+    vec2 = np.array([4, 5, 3, 7, 6])
+    vec3 = vec1 + vec2
+    print(vec3)
+
+
 def main():
- print("Hello World!")
+    listTesting()
+    numpyTesting()
+
+
 if __name__ == "__main__":
- main()
+    main()
