30ffeb33a52a4540878079461919f46a7c9795d3
fabiana.zabala <fabianazabala@hotmail.es>
2021-03-11 21:22:01 +0100

    Mean of the first column.

--- Task2.py
+++ Task2.py
@@ -8,3 +8,11 @@ data = {'total_bill': [16.99, 10.34, 21.01, 23.68, 24.59],
 df = pd.DataFrame(data, columns=['total_bill', 'tip', 'sex'])
 
 print(df)
+
+df.to_csv('file.csv', index = False)
+
+mean = df.mean()
+print('\nMean\n------')
+print(mean)
+
+df.loc[:,"total_bill"].mean()
